dd4e9054117fceb5799b348f2853672d679db917
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-14 20:56:55 +0800

    [fix] remove the login name

--- src/model/user/login/login_req.rs
+++ src/model/user/login/login_req.rs
@@ -2,13 +2,14 @@ use validator::Validate;
 
 #[derive(serde::Deserialize, Validate)]
 pub struct LoginReq {
-    pub name: String,
     #[validate(length(min = 1))]
     pub phone: String,
     #[validate(length(min = 1))]
     pub password: String,
     #[validate(length(min = 1))]
+    #[serde(rename = "appId")]
     pub app_id: String,
     #[validate(length(min = 1))]
+    #[serde(rename = "deviceId")]
     pub device_id: String,
 }
