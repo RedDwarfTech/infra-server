26d320c22c96e4c79fc381639192a5df5e0e0513
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-14 19:54:23 +0800

    [feat] add env config

---
+++ doc/apitest.md
@@ -0,0 +1,45 @@
+
+
+#### 用户登陆
+
+
+
+```bash
+
+curl 'http://localhost:8081/infra/user/login' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Accept-Language: en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7,fr;q=0.6' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/json' \
+  -H 'DNT: 1' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'x-action: LOGIN_BY_PHONE' \
+  -H 'x-request-id: 66dca516-2847-447a-b625-ddfdcdd294ce' \
+  --data-raw '{"phone":"15683761628","password":"654321","deviceId":"36d70fa8fc9e46f435bcd38ec41b86e9","deviceName":"36d70fa8fc9e46f435bcd38ec41b86e9","deviceType":4,"appId":"n29Pa29WS1","loginType":1,"cfToken":"0.hkQia4lneDHjxzDpPqHpaR1YIywjrx_nlBDzQTidD1MJ28Ok-UUIfEIzLYwRURhnvt2gCBs_3S1UnP6gUdOi43jwNqeU8YPZtg7KCF0UfV2lTa6mAfqkai9VOCYKVE2CkyNvxCEaAkI7ofTx4-BmcmLi8zZSpXekOgF24eEJBjo6g7BpvUp_BM7YPjxbIHOWFrGcfP9itxdyvoxE4EHSkN1S2Xk6SfsqE-29_eXueejSrF00bhQDGkifaQcnTXJT22WGNPC5zqx_lOzl-AOjTjmCF6Z8o6Rb7d-Ro39p4mASB8NwoGGdGH_gH5nJTknoueWGJMzR2J16GJu8R5PaYgTObmV9Vtotj83WazR6ptlakKmwVBMF-AAEm7CIn0kvAzY66MqnCLAr5EHEFeWl9CRpYMsxu2n-EWxAm4P3Yuf4vPiaTHYbReW1yyQ5zlHH.jJ8dZ78kyMgtNqUqzTCtIw.bf8bc985bdb3bd4e5fa5b61f468fabc317d9b82f999a584599773023d1b20263"}'
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/controller/user/user_controller.rs
+++ src/controller/user/user_controller.rs
@@ -1,10 +1,9 @@
-use super::rd_user::RdUser;
 use crate::model::diesel::dolphin::custom_dolphin_models::User;
 use crate::service::app::app_service::query_app_by_app_id;
 use crate::{
     model::user::login::login_req::LoginReq, service::user::user_service::query_user_by_product_id,
 };
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, Responder};
 use rust_wheel::common::util::security_util::get_sha;
 use rust_wheel::common::wrapper::actix_http_resp::box_actix_rest_response;
 use rust_wheel::common::wrapper::actix_http_resp::box_error_actix_rest_response;
@@ -18,40 +17,45 @@ pub struct FileQueryParams {
     pub file_id: String,
 }
 
-struct UserController {}
-
-impl RdUser for UserController {
-    fn login(req: actix_web_validator::Json<LoginReq>) -> HttpResponse {
-        let app_info = query_app_by_app_id(&req.0.app_id);
-        let single_user: User = query_user_by_product_id(&req.0, &app_info.product_id);
-        let pwd_salt = single_user.salt;
-        let sha_password = get_sha(String::from(&req.password), &pwd_salt);
-        if sha_password.eq(&single_user.pwd.as_str()) {
-            let rd_user = RdUserInfo{
-                id: single_user.id,
-                nickname: single_user.nickname.to_string(),
-                device_id: req.0.device_id,
-                app_id: req.0.app_id,
-            };
-            let uuid = Uuid::new_v4();
-            let access_token = create_access_token(&rd_user);
-            let login_resp = LoginResponse{ 
-                registerTime: single_user.register_time, 
-                refreshToken: uuid.to_string(), 
-                accessToken: access_token,
-                nickname: single_user.nickname.to_string()
-            };
-            return box_actix_rest_response(login_resp);
-        } else {
-            return box_error_actix_rest_response("LOGIN_INFO_NOT_MATCH", "0030010001".to_owned(), "登录信息不匹配".to_owned());
-        }
-    }
-}
-
 pub async fn get_file(_params: web::Query<FileQueryParams>) -> impl Responder {
     box_actix_rest_response("ok")
 }
 
+// https://stackoverflow.com/questions/72748775/error-the-trait-handler-is-not-implemented-for-fn-httpresponse
+pub async fn login(form: actix_web_validator::Json<LoginReq>) -> impl Responder {
+    let app_info = query_app_by_app_id(&form.0.app_id);
+    let single_user: User = query_user_by_product_id(&form.0, &app_info.product_id);
+    let pwd_salt = single_user.salt;
+    let sha_password = get_sha(String::from(&form.password), &pwd_salt);
+    if sha_password.eq(&single_user.pwd.as_str()) {
+        let rd_user = RdUserInfo {
+            id: single_user.id,
+            nickname: single_user.nickname.to_string(),
+            device_id: form.0.device_id,
+            app_id: form.0.app_id,
+        };
+        let uuid = Uuid::new_v4();
+        let access_token = create_access_token(&rd_user);
+        let login_resp = LoginResponse {
+            registerTime: single_user.register_time,
+            refreshToken: uuid.to_string(),
+            accessToken: access_token,
+            nickname: single_user.nickname.to_string(),
+        };
+        return box_actix_rest_response(login_resp);
+    } else {
+        return box_error_actix_rest_response(
+            "LOGIN_INFO_NOT_MATCH",
+            "0030010001".to_owned(),
+            "登录信息不匹配".to_owned(),
+        );
+    }
+}
+
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/infra/user").route("/list", web::get().to(get_file)));
+    cfg.service(
+        web::scope("/infra/user")
+            .route("/list", web::get().to(get_file))
+            .route("/login", web::put().to(login)),
+    );
 }
