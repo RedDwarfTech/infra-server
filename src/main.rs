b722352a42cc1751314a45fb3d0677ab9f30454c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-04-20 22:44:12 +0800

    feat: add health check entry

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate diesel;
 use crate::controller::user::user_controller;
 use actix_web::App;
 use actix_web::HttpServer;
+use controller::monitor::health_controller;
 use rust_wheel::config::app::app_conf_reader::get_app_config;
 
 pub mod controller;
@@ -18,6 +19,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .configure(user_controller::config)
+            .configure(health_controller::config)
     })
     .workers(3)
     .bind(address)?
