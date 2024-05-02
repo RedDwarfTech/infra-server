b3af5f1f1b67123ff44e8df602baeee2d1315c27
jiang qiang <jiangtingqiang@gmail.com>
2024-05-02 20:45:24 +0800

    feat: tweak alipay notification

---
+++ doc/api/pay/alipay/alipay_callback.md
@@ -0,0 +1,19 @@
+
+
+
+```bash
+curl -g -X POST https://localhost:8081/infra/alipay/notification/v1/alipaySeverNotification?voucher_detail_list=[{"amount":"0.20","merchantContribute":"0.00","name":"5折券","otherContribute":"0.20","type":"ALIPAY_DISCOUNT_VOUCHER","voucherId":"2016101200073002586200003BQ4"}]
+```
+
+
+```bash
+curl -g -X POST https://localhost:8081/infra/alipay/notification/v1/alipaySeverNotification?voucher_detail_list=[{"amount":"0.20","merchantContribute":"0.00","name":"5折券","otherContribute":"0.20","type":"ALIPAY_DISCOUNT_VOUCHER","voucherId":"2016101200073002586200003BQ4"}]\&fund_bill_list=[{"amount":"0.80","fundChannel":"ALIPAYACCOUNT"},{"amount":"0.20","fundChannel":"MDISCOUNT"}]\&subject=PC网站支付交易\&trade_no=2016101221001004580200203978\&gmt_create=2016-10-12 21:36:12\&notify_type=trade_status_sync&total_amount=1.00&out_trade_no=mobile_rdm862016-10-12213600&invoice_amount=0.80&seller_id=2088201909970555&notify_time=2016-10-12 21:41:23&trade_status=TRADE_SUCCESS&gmt_payment=2016-10-12 21:37:19&receipt_amount=0.80&passback_params=passback_params123&buyer_id=2088102114562585&app_id=2016092101248425&notify_id=7676a2e1e4e737cff30015c4b7b55e3kh6& sign_type=RSA2&buyer_pay_amount=0.80&sign=&point_amount=0.00
+```
+
+
+
+
+
+
+
+
--- src/controller/pay/alipay/alipay_notify_controller.rs
+++ src/controller/pay/alipay/alipay_notify_controller.rs
@@ -6,6 +6,12 @@ use crate::model::pay::callback::alipay_callback::AlipayCallback;
 /// Recieve notifycation
 ///
 /// Recieve notifycation
+/// The alipay will send a post request
+/// but the params will put into request params
+/// the request will look like this:
+/// https: //商家网站通知地址?voucher_detail_list=[{"amount":"0.20","merchantContribute":"0.00","name":"5折券","otherContribute":"0.20","type":"ALIPAY_DISCOUNT_VOUCHER","voucherId":"2016101200073002586200003BQ4"}]&fund_bill_list=[{"amount":"0.80","fundChannel":"ALIPAYACCOUNT"},{"amount":"0.20","fundChannel":"MDISCOUNT"}]&subject=PC网站支付交易&trade_no=2016101221001004580200203978&gmt_create=2016-10-12 21:36:12&notify_type=trade_status_sync&total_amount=1.00&out_trade_no=mobile_rdm862016-10-12213600&invoice_amount=0.80&seller_id=2088201909970555&notify_time=2016-10-12 21:41:23&trade_status=TRADE_SUCCESS&gmt_payment=2016-10-12 21:37:19&receipt_amount=0.80&passback_params=passback_params123&buyer_id=2088102114562585&app_id=2016092101248425&notify_id=7676a2e1e4e737cff30015c4b7b55e3kh6& sign_type=RSA2&buyer_pay_amount=0.80&sign=***&point_amount=0.00
+/// so we still need to parse the params from url 
+/// not from request body
 #[utoipa::path(
     context_path = "/infra/alipay/pay",
     path = "/",
@@ -14,9 +20,9 @@ use crate::model::pay::callback::alipay_callback::AlipayCallback;
     )
 )]
 #[post("/v1/alipaySeverNotification")]
-pub async fn alipay_server_notify(payload: web::Json<AlipayCallback>) -> impl Responder {
-    warn!("receive alipay callback, payload: {:?}", payload.0);
-    handle_pay_callback(&payload.0);
+pub async fn alipay_server_notify(req: HttpRequest) -> impl Responder {
+    warn!("receive alipay callback, params: {:?}", req.query_string());
+    // handle_pay_callback(&payload.0);
     return HttpResponse::Unauthorized().finish();
 }
 
