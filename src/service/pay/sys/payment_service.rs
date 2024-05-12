b4d393a54764e8a37ada6bc45d8b52b441c3723c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-05-12 22:45:50 +0800

    refactor: insert on conflict

--- src/service/pay/sys/payment_service.rs
+++ src/service/pay/sys/payment_service.rs
@@ -1,5 +1,6 @@
 use diesel::{PgConnection, RunQueryDsl};
 use log::error;
+use crate::diesel::OptionalExtension;
 
 use crate::model::diesel::{
     custom::pay::payment_add::PaymentAdd, dolphin::custom_dolphin_models::Payment,
@@ -7,10 +8,12 @@ use crate::model::diesel::{
 
 pub fn save_payment(payments: &PaymentAdd, connection: &mut PgConnection) {
     use crate::model::diesel::dolphin::dolphin_schema::payments as payment_table;
+    // https://github.com/diesel-rs/diesel/issues/952
     let insert_result = diesel::insert_into(payment_table::dsl::payments)
         .values(payments)
         .on_conflict_do_nothing()
-        .get_result::<Payment>(connection);
+        .get_result::<Payment>(connection)
+        .optional();
     if let Err(err) = insert_result {
         error!("insert payments facing issue: {}", err);
     }
