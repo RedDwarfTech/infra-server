47b937c71707adfab53483789477f5b6ee585743
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-05-12 21:12:53 +0800

    feat: payment conflicts handle

--- src/service/pay/sys/payment_service.rs
+++ src/service/pay/sys/payment_service.rs
@@ -8,6 +8,7 @@ pub fn save_payment(payments: &PaymentAdd, connection: &mut PgConnection) {
     use crate::model::diesel::dolphin::dolphin_schema::payments as payment_table;
     diesel::insert_into(payment_table::dsl::payments)
         .values(payments)
+        .on_conflict_do_nothing()
         .get_result::<Payment>(connection)
         .expect("failed to add new order");
 }
