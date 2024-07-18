1fc8079d00df622ff9122c321caa30c394b0278d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-07-18 23:02:03 +0800

    refactor: tweak the sms log insert

--- src/service/notify/sms_log_service.rs
+++ src/service/notify/sms_log_service.rs
@@ -1,3 +1,5 @@
+use log::error;
+
 use crate::common::db::database::get_conn;
 use crate::diesel::RunQueryDsl;
 use crate::model::diesel::custom::notify::sms_log_add::SmsLogAdd;
@@ -5,8 +7,10 @@ use crate::model::diesel::dolphin::custom_dolphin_models::SmsLog;
 
 pub fn save_sms_log(sms_log: &SmsLogAdd) {
     use crate::model::diesel::dolphin::dolphin_schema::sms_log as query_table;
-    diesel::insert_into(query_table::dsl::sms_log)
+    let result = diesel::insert_into(query_table::dsl::sms_log)
         .values(sms_log)
-        .get_result::<SmsLog>(&mut get_conn())
-        .expect("sms log add failed");
+        .get_result::<SmsLog>(&mut get_conn());
+    if let Err(e) = result {
+        error!("insert sms log failed: {:?}, sms log: {:?}", e, sms_log);
+    }
 }
